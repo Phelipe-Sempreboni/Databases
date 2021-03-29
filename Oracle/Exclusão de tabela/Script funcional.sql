926ae6d646e07dd97be86b27996bedc23935c058
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2021-03-29 08:34:17 -0300

    Criação da pasta com exemplo de exclusão de tabela.
    Criação da pasta com exemplo de exclusão de tabela com exemplo de script de criação e de exclusão.

---
+++ Oracle/Exclusão de tabela/Script funcional.sql
@@ -0,0 +1,22 @@
+-- Comando para exlusão de tabela no Oracle Database pelo SQL Developer ou SQL Plus.
+-- Este comando exlui a tabela inteira, onde devemos ter muito cuidado com este comando, pois, apaga a tabela inteira.
+-- Iremos utilizar como exemplo o script de criação de tabela juntamente do DROP, para termos noção do que está sendo apagado.
+
+-- Este script funcional está no exemplo da pasta (Criação de tabela).
+CREATE TABLE TBL_CADASTRO_CLIENTES (
+     CPF VARCHAR (11)
+    ,NOME_COMPLETO VARCHAR (150)
+    ,RUA VARCHAR (100)
+    ,BAIRRO VARCHAR (100)
+    ,CIDADE VARCHAR (60)
+    ,ESTADO VARCHAR (5)
+    ,CEP VARCHAR (10)
+    ,IDADE INT
+    ,SEXO VARCHAR (20)
+    ,LIMITE_CREDITO_COMPRA FLOAT
+    ,VOLUME_MINIMO_COMPRA FLOAT
+    ,PRIMEIRA_COMPRA NUMBER (1)
+);
+
+-- Comando para exclusão da tabela que foi criada acima.
+DROP TABLE TBL_CADASTRO_CLIENTES;
