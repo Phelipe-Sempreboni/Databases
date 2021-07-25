22d082825569a3d50011559d4e9822c6faf92ed4
Phelipe-Sempreboni <57469401+Phelipe-Sempreboni@users.noreply.github.com>
2021-07-24 22:12:05 -0300

    Criação do repositório (Function de criação de calendário em português) e script da function.

--- SQL Server/Function de criação de calendário em português/Script funcional.sql
+++ SQL Server/Function de criação de calendário em português/Script funcional.sql
@@ -1,4 +1,4 @@
--- Criação de uma function para criação de um calendário em inglês.
+-- Criação de uma function para criação de um calendário em português.
 -- Teremos os comandos:
 -- Criação de um database.
 -- Criação de um schema.
@@ -25,7 +25,7 @@ GO
 --CREATE SCHEMA CALENDARIO;
 --GO
  
- CREATE FUNCTION CALENDARIO.FN_CALENDARIO_PADRÃO_ES_US
+ CREATE FUNCTION CALENDARIO.FN_CALENDARIO_PADRÃO_PT_BR
  (
 	 @DATA1 DATE
 	,@DATA2 DATE
@@ -34,18 +34,39 @@ GO
  AS
  RETURN
  (
+ WITH 
+	[Calendario1] AS (
  SELECT 
-	CONVERT(DATE, DATEADD(DAY, NUMBER + 1, @DATA1), 103) AS [DATA]
+	CONVERT(DATE, DATEADD(DAY, NUMBER + 1, @DATA1), 103) AS [data]
 	,
 	CASE
 		WHEN CONVERT(DATE, DATEADD(DAY, NUMBER + 1, @DATA1), 103) NOT IN (' ') THEN DATENAME(DW,CONVERT(DATE, DATEADD(DAY, NUMBER + 1, @DATA1), 103))
-	END AS [DIA_DA_SEMANA_INGLES]
+	END AS [DIA_DA_SEMANA_PORTUGUES]
 
 	FROM master..spt_values AS A
 
 	WHERE type = 'P' AND DATEADD(DAY, NUMBER + 1, @DATA1) < @DATA2
- );
- GO
+),
+	[Calendario2] AS (
+ SELECT
+	[data]
+   ,
+
+   CASE
+		WHEN [DIA_DA_SEMANA_PORTUGUES] = 'Monday' THEN 'Segunda-feira'
+		WHEN [DIA_DA_SEMANA_PORTUGUES] = 'Tuesday' THEN 'Terça-feira'
+		WHEN [DIA_DA_SEMANA_PORTUGUES] = 'Wednesday' THEN 'Quarta-feira'
+		WHEN [DIA_DA_SEMANA_PORTUGUES] = 'Thursday' THEN 'Quinta-feira'
+		WHEN [DIA_DA_SEMANA_PORTUGUES] = 'Friday' THEN 'Sexta-feira'
+		WHEN [DIA_DA_SEMANA_PORTUGUES] = 'Saturday' THEN 'Sábado'
+		WHEN [DIA_DA_SEMANA_PORTUGUES] = 'Sunday' THEN 'Domingo'
+   END AS [DIA_DA_SEMANA_PORTUGUÊS]
+
+ FROM [Calendario1]
+)
+SELECT * FROM [Calendario2]
+);
+GO
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
