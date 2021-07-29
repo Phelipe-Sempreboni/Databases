a44cf834da830419f4199a1c700245dea493525e
Phelipe-Sempreboni <57469401+Phelipe-Sempreboni@users.noreply.github.com>
2021-07-29 15:12:17 -0300

    Atualização de script.

--- Oracle/Verificação de infos das tabelas que o user tem permissão/Script funcional.sql
+++ Oracle/Verificação de infos das tabelas que o user tem permissão/Script funcional.sql
@@ -1,3 +1,4 @@
 -- Comando para verificação infos de owner, tablename, tablespace, cluster, etc.
+-- ALL_TABLES - Permite ao usuário que está realizando a consulta visualizar as tabelas em que este usuário tem permissão.
 
 SELECT * FROM ALL_TABLES;
