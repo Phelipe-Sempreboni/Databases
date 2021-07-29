aaf87d4f207f23466a009ca5753e3f717c3a529f
Phelipe-Sempreboni <57469401+Phelipe-Sempreboni@users.noreply.github.com>
2021-07-29 15:11:48 -0300

    Atualização de script.

--- Oracle/Verificação de infos de todas as tabelas do banco de dados/Script funcional.sql
+++ Oracle/Verificação de infos de todas as tabelas do banco de dados/Script funcional.sql
@@ -1,3 +1,4 @@
 -- Comando para verificação infos de owner, tablename, tablespace, cluster, etc.
+-- DBA_TABLES - Contem as informações sobre todas as tabelas do banco de dados.
 
 SELECT * FROM  DBA_TABLES;
