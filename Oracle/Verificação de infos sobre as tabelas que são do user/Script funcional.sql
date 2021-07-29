18c83299bd88d4deb9887174f2b4772a115bb8cc
Phelipe-Sempreboni <57469401+Phelipe-Sempreboni@users.noreply.github.com>
2021-07-29 15:11:13 -0300

    Criação do repositório (Verificação de infos sobre as tabelas que são do user) e criação do script.

---
+++ Oracle/Verificação de infos sobre as tabelas que são do user/Script funcional.sql
@@ -0,0 +1,4 @@
+-- Comando para verificação infos de owner, tablename, tablespace, cluster, etc.
+-- USER_TABLES - Contem as informações sobre as tabelas que pertencem ao usuário que está realizando a consulta.
+
+SELECT * FROM USER_TABLES;
