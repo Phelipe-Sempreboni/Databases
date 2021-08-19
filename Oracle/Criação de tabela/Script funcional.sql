556cb06dcd2091e932fb7d344247791459b5d8a1
Phelipe-Sempreboni <57469401+Phelipe-Sempreboni@users.noreply.github.com>
2021-08-18 22:21:43 -0300

    Atualização do script de criação de tabelas.

--- Oracle/Criação de tabela/Script funcional.sql
+++ Oracle/Criação de tabela/Script funcional.sql
@@ -144,3 +144,154 @@ ALTER TABLE TBL_PRODUTOS ADD DATA_VALIDADE DATE;
 SELECT * FROM TBL_PRODUTOS;
 
 ------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x
+
+-- Oitavo exemplo:
+-- Construção de uma tabela com: 
+-- Coluna com auto incremento automático, neste caso um tipo de dado (INT) inteiro.
+-- Chave primária.
+-- Constraint (restrição).
+-- Comentários na tabea e colunas.
+
+CREATE TABLE PRODUTOS.ETL_SAP_ESTOQUE
+(
+     ID_ETL_SAP_ESTOQUE INT GENERATED ALWAYS AS IDENTITY MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CONSTRAINT PK_ETL_API_PEOPLES PRIMARY KEY
+    ,ID_ORIGIN_SAP_ESTOQUE VARCHAR(50)
+    ,DES_MATERIAL_NAME VARCHAR (100)
+    ,DES_DESCRIPTION_MATERIAL VARCHAR (100)
+    ,NAM_OWNER VARCHAR (100)
+
+);
+
+COMMENT ON COLUMN PRODUTOS.ETL_SAP_ESTOQUE.ID_ETL_SAP_ESTOQUE IS 'Coluna de id gerada automaticamente com autoincremento.'; -- Adicição do comentário na coluna.
+COMMENT ON COLUMN PRODUTOS.ETL_SAP_ESTOQUE.ID_ORIGIN_SAP_ESTOQUE IS 'Coluna com id de origem do sistema.'; -- Adicição do comentário na coluna.
+COMMENT ON COLUMN PRODUTOS.ETL_SAP_ESTOQUE.DES_MATERIAL_NAME IS 'Coluna com o nome do material.'; -- Adicição do comentário na coluna.
+COMMENT ON COLUMN PRODUTOS.ETL_SAP_ESTOQUE.DES_DESCRIPTION_MATERIAL IS 'Coluna com a descrição do material.'; -- Adicição do comentário na coluna.
+COMMENT ON COLUMN PRODUTOS.ETL_SAP_ESTOQUE.NAM_OWNER IS 'Coluna com o responsável pelo material.'; -- Adicição do comentário na coluna.
+COMMENT ON TABLE PRODUTOS.ETL_SAP_ESTOQUE IS 'Coluna com a informação da quantidade em estoque disponível.'; -- Adicição do comentário na tabela.
+
+------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x
+
+-- Oitavo exemplo:
+-- Construção de uma tabela com: 
+-- Coluna com auto incremento automático, neste caso um tipo de dado (INT) inteiro.
+-- Chave primária.
+-- Constraint (restrição).
+-- Comentários na tabea e colunas.
+
+-- Neste caso abaixo, é um script que antes de criar a tabela, ele verifica se ela não existe, e caso não exista é criada, caso contrátio, será mantida a tabela existente e uma mensagem será exibida que a tabela já existe.
+
+SET ECHO ON
+
+SET SERVEROUT ON
+
+DECLARE
+    V_ERROR_RESOURCE_BUSY EXCEPTION;
+    PRAGMA EXCEPTION_INIT(V_ERROR_RESOURCE_BUSY, -00054);
+    V_SQL1 CONSTANT VARCHAR2(2000) := 'CREATE TABLE PRODUTOS.ETL_SAP_ESTOQUE(
+     ID_ETL_SAP_ESTOQUE INT GENERATED ALWAYS AS IDENTITY MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CONSTRAINT PK_ETL_API_PEOPLES PRIMARY KEY
+    ,ID_ORIGIN_SAP_ESTOQUE VARCHAR(50)
+    ,DES_MATERIAL_NAME VARCHAR (100)
+    ,DES_DESCRIPTION_MATERIAL VARCHAR (100)
+    ,NAM_OWNER VARCHAR (100)
+)';
+    V_NAOEXECUTOU BOOLEAN := TRUE;
+    V_EXISTE      NUMBER(10) := 0;
+    V_TIME INT:=1;
+    V_NOW DATE;
+BEGIN
+    SELECT COUNT(1)
+        INTO V_EXISTE
+    FROM ALL_TABLES
+    WHERE OWNER = 'PRODUTOS' -- Escreva o schema/usuário
+    AND TABLE_NAME = 'ETL_SAP_ESTOQUE' -- Escreva o nome da tabela
+    ;
+   IF V_EXISTE > 0 THEN
+      DBMS_OUTPUT.PUT_LINE('*** Essa tabela já existe neste ambiente: ' || SYS_CONTEXT('USERENV','INSTANCE_NAME') || ' / ' || SYS_CONTEXT('USERENV','SERVER_HOST'));
+   ELSE
+      WHILE V_NAOEXECUTOU
+      LOOP
+         BEGIN
+            EXECUTE IMMEDIATE V_SQL1;
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN PRODUTOS.ETL_SAP_ESTOQUE.ID_ETL_SAP_ESTOQUE IS ''Coluna de id gerada automaticamente com autoincremento.'''; -- Adicição do comentário na coluna.
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN PRODUTOS.ETL_SAP_ESTOQUE.ID_ORIGIN_SAP_ESTOQUE IS ''Coluna com id de origem do sistema..'''; -- Adicição do comentário na coluna.
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN PRODUTOS.ETL_SAP_ESTOQUE.DES_MATERIAL_NAME IS ''Coluna com o nome do material.'''; -- Adicição do comentário na coluna.
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN PRODUTOS.ETL_SAP_ESTOQUE.DES_DESCRIPTION_MATERIAL IS ''Coluna com a descrição do material.'''; -- Adicição do comentário na coluna.
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN PRODUTOS.ETL_SAP_ESTOQUE.NAM_OWNER IS ''Coluna com o responsável pelo material.'''; -- Adicição do comentário na coluna.
+            EXECUTE IMMEDIATE 'COMMENT ON TABLE PRODUTOS.ETL_SAP_ESTOQUE IS ''Coluna com a informação da quantidade em estoque disponível.'''; -- Adicição do comentário na tabela.
+            V_NAOEXECUTOU := FALSE;
+            DBMS_OUTPUT.PUT_LINE('--');
+            DBMS_OUTPUT.PUT_LINE(V_SQL1);
+            DBMS_OUTPUT.PUT_LINE('--');
+         EXCEPTION
+            WHEN V_ERROR_RESOURCE_BUSY THEN
+                SELECT SYSDATE INTO V_NOW FROM DUAL;
+                LOOP
+                EXIT WHEN V_NOW + (V_TIME * (1/86400)) = SYSDATE;
+                END LOOP;
+            WHEN OTHERS THEN
+               RAISE;
+         END;
+      END LOOP;
+   END IF;
+END;
+/
+------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x
+
+-- Este script é a mesma coisa do script acima, porém, fizemos como se o schema/usuário fosse o próprio padrão do Oracle Database, o SYSTEM.
+
+SET ECHO ON
+
+SET SERVEROUT ON
+
+DECLARE
+    V_ERROR_RESOURCE_BUSY EXCEPTION;
+    PRAGMA EXCEPTION_INIT(V_ERROR_RESOURCE_BUSY, -00054);
+    V_SQL1 CONSTANT VARCHAR2(2000) := 'CREATE TABLE SYSTEM.ETL_SAP_ESTOQUE(
+     ID_ETL_SAP_ESTOQUE INT GENERATED ALWAYS AS IDENTITY MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CONSTRAINT PK_ETL_API_PEOPLES PRIMARY KEY
+    ,ID_ORIGIN_SAP_ESTOQUE VARCHAR(50)
+    ,DES_MATERIAL_NAME VARCHAR (100)
+    ,DES_DESCRIPTION_MATERIAL VARCHAR (100)
+    ,NAM_OWNER VARCHAR (100)
+)';
+    V_NAOEXECUTOU BOOLEAN := TRUE;
+    V_EXISTE      NUMBER(10) := 0;
+    V_TIME INT:=1;
+    V_NOW DATE;
+BEGIN
+    SELECT COUNT(1)
+        INTO V_EXISTE
+    FROM ALL_TABLES
+    WHERE OWNER = 'SYSTEM' -- Escreva o schema/usuário
+    AND TABLE_NAME = 'ETL_SAP_ESTOQUE' -- Escreva o nome da tabela
+    ;
+   IF V_EXISTE > 0 THEN
+      DBMS_OUTPUT.PUT_LINE('*** Essa tabela já existe neste ambiente: ' || SYS_CONTEXT('USERENV','INSTANCE_NAME') || ' / ' || SYS_CONTEXT('USERENV','SERVER_HOST'));
+   ELSE
+      WHILE V_NAOEXECUTOU
+      LOOP
+         BEGIN
+            EXECUTE IMMEDIATE V_SQL1;
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN SYSTEM.ETL_SAP_ESTOQUE.ID_ETL_SAP_ESTOQUE IS ''Coluna de id gerada automaticamente com autoincremento.'''; -- Adicição do comentário na coluna.
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN SYSTEM.ETL_SAP_ESTOQUE.ID_ORIGIN_SAP_ESTOQUE IS ''Coluna com id de origem do sistema..'''; -- Adicição do comentário na coluna.
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN SYSTEM.ETL_SAP_ESTOQUE.DES_MATERIAL_NAME IS ''Coluna com o nome do material.'''; -- Adicição do comentário na coluna.
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN SYSTEM.ETL_SAP_ESTOQUE.DES_DESCRIPTION_MATERIAL IS ''Coluna com a descrição do material.'''; -- Adicição do comentário na coluna.
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN SYSTEM.ETL_SAP_ESTOQUE.NAM_OWNER IS ''Coluna com o responsável pelo material.'''; -- Adicição do comentário na coluna.
+            EXECUTE IMMEDIATE 'COMMENT ON COLUMN TABLE SYSTEM.ETL_SAP_ESTOQUE IS ''Coluna com a informação da quantidade em estoque disponível.'''; -- Adicição do comentário na tabela.
+            V_NAOEXECUTOU := FALSE;
+            DBMS_OUTPUT.PUT_LINE('--');
+            DBMS_OUTPUT.PUT_LINE(V_SQL1);
+            DBMS_OUTPUT.PUT_LINE('--');
+         EXCEPTION
+            WHEN V_ERROR_RESOURCE_BUSY THEN
+                SELECT SYSDATE INTO V_NOW FROM DUAL;
+                LOOP
+                EXIT WHEN V_NOW + (V_TIME * (1/86400)) = SYSDATE;
+                END LOOP;
+            WHEN OTHERS THEN
+               RAISE;
+         END;
+      END LOOP;
+   END IF;
+END;
+/
+------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x------ x
