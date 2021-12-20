649af40b4173f1c6591c5adf3ce5a524f3fd070f
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2021-12-20 11:49:50 -0300

    Criação do repositório (procedures).

---
+++ oracle/procedures/script.sql
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------------------------------------
+
+-- Query para listar as functions, packages, procedures, trigger, type e mais funções do banco de dados.
+
+----------------------------------------------------------------------------------------------------------------
+
+-- Lista todas as funções e procedimentos, junto com as propriedades associadas.
+SELECT * FROM DBA_PROCEDURES;
+
+----------------------------------------------------------------------------------------------------------------
+
+-- Lista todas as funções e procedimentos, junto com as propriedades associadas. Não contém a coluna OWNER.
+SELECT * FROM USER_PROCEDURES;
+
+----------------------------------------------------------------------------------------------------------------
+
+-- Lista todas as funções e procedimentos, junto com as propriedades associadas.
+-- Neste caso abaixo, listamos o objeto do tipo (PROCEDURE) do usuário (SYS).
+SELECT * FROM DBA_PROCEDURES
+WHERE OBJECT_TYPE = 'PROCEDURE' AND OWNER = 'SYS'
+ORDER BY OBJECT_NAME;
+
+----------------------------------------------------------------------------------------------------------------
+
+-- Agrupamento para verificar os tipos de objetos que é possível verificar com a (DBA_PROCEDURES).
+SELECT
+      OBJECT_TYPE
+FROM DBA_PROCEDURES
+GROUP BY
+        OBJECT_TYPE
+ORDER BY
+        OBJECT_TYPE;
+
+----------------------------------------------------------------------------------------------------------------
+
+-- Script para executar uma procedure com mais eficácia.
+-- Substitua os campos (SCHEMA e PROCEDURE).
+SET ECHO ON
+SET SERVEROUTPUT ON
+BEGIN
+SCHEMA.PROCEDURE (NULL);
+END;
+/
+
+----------------------------------------------------------------------------------------------------------------
