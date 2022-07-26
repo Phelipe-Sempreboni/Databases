abfa618ae5665f66f3857cf4a01f2d436b62cd96
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2022-07-26 20:58:38 -0300

    Atualização de script.

--- trainings/data science academy/sql for data science/3 - categorization, encoding and binarization of variables with sql/6 - script_queries.sql
+++ trainings/data science academy/sql for data science/3 - categorization, encoding and binarization of variables with sql/6 - script_queries.sql
@@ -1,1 +1,36 @@
+---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x 
+
 SELECT * FROM dsa_module_three.tb_dados;
+
+---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x 
+
+SELECT COUNT(*) FROM dsa_module_three.tb_dados;
+
+---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x 
+
+SELECT DISTINCT classe FROM dsa_module_three.tb_dados;
+
+---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x 
+
+# Binarização da variável classe (0/1)
+
+SELECT * 
+,
+CASE
+	WHEN classe = "no-recurrence-events" THEN 0
+    WHEN classe = "recurrence-events" THEN 1
+    END AS teste
+    
+FROM dsa_module_three.tb_dados;
+
+---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x 
+
+
+
+
+
+
+
+
+
+
