c11513933a992305ce98c89f1b34a33aaed8b3af
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2022-07-26 21:41:32 -0300

    Atualização de script sql.

--- trainings/data science academy/sql for data science/3 - categorization, encoding and binarization of variables with sql/6 - script_queries.sql
+++ trainings/data science academy/sql for data science/3 - categorization, encoding and binarization of variables with sql/6 - script_queries.sql
@@ -104,8 +104,18 @@ FROM dsa_module_three.tb_dados;
 
 ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x 
 
+SELECT *
+,
+	CASE 
+		WHEN tamanho_tumor = '0-4' OR tamanho_tumor = '5-9' THEN 'Muito Pequeno'
+        WHEN tamanho_tumor = '10-14' OR tamanho_tumor = '15-19' THEN 'Pequeno'
+        WHEN tamanho_tumor = '20-24' OR tamanho_tumor = '25-29' THEN 'Medio'
+        WHEN tamanho_tumor = '30-34' OR tamanho_tumor = '35-39' THEN 'Grande'
+        WHEN tamanho_tumor = '40-44' OR tamanho_tumor = '45-49' THEN 'Muito Grande'
+        WHEN tamanho_tumor = '50-54' OR tamanho_tumor = '55-59' THEN 'Tratamento Urgente'
+        
+	END AS tamanho_tumor_cat
 
-
-
+FROM dsa_module_three.tb_dados;
 
 ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x ---- x 
