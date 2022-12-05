bf3a4127c117296d3d9e7052151d79b0988c0cf6
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2022-12-05 20:59:10 -0300

    Exercicios de SQL.

---
+++ trainings/data science academy/sql for data science/3 - categorization, encoding and binarization of variables with sql/11 - sql exercise.sql
@@ -0,0 +1,79 @@
+------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # 
+
+SELECT * FROM dsa_module_three.tb_dados2;
+
+------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # 
+
+SELECT DISTINCT MENOPAUSA FROM dsa_module_three.tb_dados2;
+
+SELECT DISTINCT DEG_MALIG FROM dsa_module_three.tb_dados2;
+
+------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # 
+
+SELECT 
+	 *
+	,
+	 CASE
+	 WHEN menopausa = 'ge40' THEN 1
+     WHEN menopausa = 'lt40' THEN 2
+     WHEN menopausa = 'premeno' THEN 3
+     END AS MENOPAUSA_LABEL
+
+FROM dsa_module_three.tb_dados2;
+
+------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # 
+
+SELECT 
+	 CLASSE
+	,IDADE
+    ,MENOPAUSA
+	,TAMANHO_TUMOR
+    ,INV_NODES
+    ,NODE_CAPS
+    ,DEG_MALIG
+    ,SEIO
+    ,QUADRANTE
+    ,IRRADIANDO
+	,CONCAT(INV_NODES, QUADRANTE) AS posicao_tumor
+	,
+	CASE
+	WHEN menopausa = 'ge40' THEN 1
+	WHEN menopausa = 'lt40' THEN 2
+	WHEN menopausa = 'premeno' THEN 3
+	END AS menopausa_label
+
+FROM dsa_module_three.tb_dados2;
+
+------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # 
+
+SELECT 
+	 CLASSE
+	,IDADE
+    ,MENOPAUSA
+	,TAMANHO_TUMOR
+    ,INV_NODES
+    ,NODE_CAPS
+    ,DEG_MALIG
+    ,SEIO
+    ,QUADRANTE
+    ,IRRADIANDO
+	,CONCAT(INV_NODES, QUADRANTE) AS posicao_tumor
+	,
+	CASE
+	WHEN menopausa = 'ge40' THEN 1
+	WHEN menopausa = 'lt40' THEN 2
+	WHEN menopausa = 'premeno' THEN 3
+	END AS menopausa_label
+    ,
+    CASE
+    WHEN DEG_MALIG = 1 THEN 1 ELSE DEG_MALIG = 0 END AS deg_malig_label_1
+    ,
+    CASE
+    WHEN DEG_MALIG = 2 THEN 2 ELSE DEG_MALIG = 0 END AS deg_malig_label_2
+    ,
+    CASE
+    WHEN DEG_MALIG = 3 THEN 3 ELSE DEG_MALIG = 0 END AS deg_malig_label_3
+
+FROM dsa_module_three.tb_dados2;
+
+------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # ------- # 
