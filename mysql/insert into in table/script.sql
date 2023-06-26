059f209b4e4915c8f22acf299f7adab67aeb76fb
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2023-06-26 20:20:05 -0300

    Criação de arquivo.

---
+++ mysql/insert into in table/script.sql
@@ -0,0 +1,25 @@
+------------------------------------------------------------------------------------------------------------------- #
+
+-- Exemplo de script (INSERT INTO), ou seja, para adicionar dados em uma tabela existente no MySQL.
+
+INSERT INTO projetos (nome_projeto, breve_descricao) VALUES ('ArmazemMagazine', 'Modernizacao do armazem da Magazine Luiza');
+INSERT INTO projetos (nome_projeto, breve_descricao) VALUES ('Ecovias', 'Reestruturacao de Data Warehouse (DW)');
+INSERT INTO projetos (nome_projeto, breve_descricao) VALUES ('ProjetoNet', 'Construção do novo Data lake da NET em Perdizes');
+INSERT INTO projetos (nome_projeto, breve_descricao) VALUES ('ProjetoClaro', 'Construção do novo Data lake da Claro em Guarulhos');
+
+------------------------------------------------------------------------------------------------------------------- #
+
+INSERT INTO funcionarios (nome_funcionario) VALUES ('Henrique Castilho');
+INSERT INTO funcionarios (nome_funcionario) VALUES ('Grazielle Azevedo');
+
+------------------------------------------------------------------------------------------------------------------- #
+
+INSERT INTO cargos (nome_cargo, salario_cargo) VALUES ('Analista de Negócios', 10000);
+INSERT INTO cargos (nome_cargo, salario_cargo) VALUES ('Analista de Planejamento e Controle', 10000);
+
+------------------------------------------------------------------------------------------------------------------- #
+
+INSERT INTO setores (nome_setor) VALUES ('Network Global');
+INSERT INTO setores (nome_setor) VALUES ('Commercial and Faturing Global');
+
+------------------------------------------------------------------------------------------------------------------- #
