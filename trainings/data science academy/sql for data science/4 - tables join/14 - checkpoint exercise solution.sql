b96c9cc5e9564cf134f5a619e7e0f3b909a7ee6e
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2023-06-25 22:02:34 -0300

    Criação de arquivo.

---
+++ trainings/data science academy/sql for data science/4 - tables join/14 - checkpoint exercise solution.sql
@@ -0,0 +1,20 @@
+# -------------------------------------------------------------------------------------------------------------------------------------- #
+
+# Retornar a data do pedido, o nome do cliente, todos os vendedores, com ou sem pedido associado, e ordenar o resultado pelo nome do cliente.
+
+SELECT
+	 C.nome_cliente
+	,A.nome_vendedor
+	,B.data_pedido
+    
+FROM dsa_module_four.tb_vendedor AS A 
+
+LEFT JOIN dsa_module_four.tb_pedidos AS B USING (id_vendedor)
+
+LEFT JOIN dsa_module_four.tb_clientes AS C ON B.id_cliente = C.id_cliente
+
+ORDER BY
+	C.nome_cliente DESC
+;
+
+# -------------------------------------------------------------------------------------------------------------------------------------- #
