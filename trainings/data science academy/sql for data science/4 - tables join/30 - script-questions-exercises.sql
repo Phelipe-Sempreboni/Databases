f42ac45689e76132ba7876ea51680c01a1ff135b
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2023-07-02 20:38:59 -0300

    Atualização de arquivo.

--- trainings/data science academy/sql for data science/4 - tables join/30 - script-questions-exercises.sql
+++ trainings/data science academy/sql for data science/4 - tables join/30 - script-questions-exercises.sql
@@ -22,4 +22,40 @@ WHERE A.discipline = 'Handball' AND B.discipline = 'Handball'
 
 ORDER BY B.name;
 
+-- Também é possível resolver com as querys abaixo, de uma maneira bem simples.
+SELECT name FROM exec3.TB_COACHES WHERE discipline = 'Handball';
+SELECT name FROM exec3.TB_ATLETAS WHERE discipline = 'Handball';
+
+------------------------------------------------------------------------------------------------------------------------------ #
+
+-- 2 - Quem são os técnicos (coaches) dos atletas da Austrália que receberam medalhas de Ouro?
+-- Até podemos tentar chegar em um resultado, mas ainda não está correto
+-- Não pode ser respondido
+SELECT A.name, B.gold
+FROM dsa_module_four.tb_coaches AS A, dsa_module_four.tb_medals AS B
+WHERE A.noc = B.team_noc
+AND A.noc = 'Australia'
+AND gold > 0
+ORDER BY gold DESC;
+
+------------------------------------------------------------------------------------------------------------------------------ #
+
+-- 3 - Quais países tiveram mulheres conquistando medalhas de Ouro?
+-- Não pode ser respondido
+
+------------------------------------------------------------------------------------------------------------------------------ #
+
+-- 4 - Quais atletas da Noruega receberam medalhas de Ouro ou Prata?
+-- Até podemos tentar chegar em um resultado, mas ainda não está correto
+-- Não pode ser respondido
+SELECT A.noc, gold, silver 
+FROM exec3.TB_ATLETAS A, exec3.TB_MEDALS B
+WHERE A.noc = B.noc
+AND a.noc = 'Norway'
+
+------------------------------------------------------------------------------------------------------------------------------ #
+
+-- 5 - Quais atletas Brasileiros não receberam medalhas?
+-- Não pode ser respondido
+
 ------------------------------------------------------------------------------------------------------------------------------ #
