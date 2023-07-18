4fa8ace1e983b5ac7bfc6e842f9d1ac1707f1045
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2023-07-17 21:46:36 -0300

    Atualização de arquivo.

--- trainings/data science academy/sql for data science/6 - windows functions, subqueries and date handling/20 - scripts-aula.sql
+++ trainings/data science academy/sql for data science/6 - windows functions, subqueries and date handling/20 - scripts-aula.sql
@@ -8,18 +8,64 @@ FROM dsa_module_six.tb_vendas;
 
 ------------------------------------------------------------------------------------------------------------------------ #
 
+-- Total de vendas por ano fiscal
+
+SELECT
+	 ano_fiscal
+    ,SUM(valor_venda)
+    
+FROM dsa_module_six.tb_vendas
+
+GROUP BY
+	ano_fiscal
+;
 
 ------------------------------------------------------------------------------------------------------------------------ #
 
+-- Total de vendas por funcionário
 
+SELECT
+	 nome_funcionario
+    ,SUM(valor_venda)
+    
+FROM dsa_module_six.tb_vendas
+
+GROUP BY
+	nome_funcionario
+;
 
 ------------------------------------------------------------------------------------------------------------------------ #
 
+-- Total de vendas por ano fiscal e funcionário
 
+SELECT
+	 ano_fiscal
+	,nome_funcionario
+    ,SUM(valor_venda)
+    
+FROM dsa_module_six.tb_vendas
 
+GROUP BY
+	 ano_fiscal
+    ,nome_funcionario
+;
 
 ------------------------------------------------------------------------------------------------------------------------ #
 
+-- Total de vendas por ano fiscal e funcionário
+
+SELECT
+	 ano_fiscal
+	,nome_funcionario
+    ,valor_venda
+    ,SUM(valor_venda) OVER (PARTITION BY ano_fiscal) AS total_vendas_ano
+    
+FROM dsa_module_six.tb_vendas
+
+ORDER BY ano_fiscal
+;
+
+------------------------------------------------------------------------------------------------------------------------ #
 
 
 
